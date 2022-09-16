3d11f301e20dd67951d2fd1531e7f4dde82e3243
Suelem Macedo <suelemmacedo_@hotmail.com>
2022-09-15 23:24:56 -0300

    Requisito 7 concluído

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,13 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se ao executar "getSavedCartItem" o método local.Storage.GetItem é chamado', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('Testa se ao executar "getSavedCartItems" o método local.Storage.GetItem é chamado com um "cartItems" como parâmetro', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled("cartItems");
+  });
+  
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,11 +5,11 @@ const { expect } = require("@jest/globals");
 localStorageSimulator("setItem");
 
 describe("3 - Teste a função saveCartItems", () => {
-  it('Testa se ao executar "saveCartItems com um "cartItem" como argumento o método local.Storage.SetItem é chamado', () => {
+  it('Testa se ao executar "saveCartItems" com um "cartItem" como argumento o método local.Storage.SetItem é chamado', () => {
     saveCartItems('cartItem');
     expect(localStorage.setItem).toHaveBeenCalled();
   });
-  it('Testa se ao executar "saveCartItems com um "cartItem" como argumento o método local.Storage.SetItem é chamado com dois parâmetros, sendo o primeiro a chave "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+  it('Testa se ao executar "saveCartItems" com um "cartItem" como argumento o método local.Storage.SetItem é chamado com dois parâmetros, sendo o primeiro a chave "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
     saveCartItems('product');
     expect(localStorage.setItem).toHaveBeenCalledWith("cartItems", "product");
   });
